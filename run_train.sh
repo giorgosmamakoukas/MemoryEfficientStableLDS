fcdc1820eaaa971fc7ac93418ad566f6e2607af0
Orest Xherija <orest.xherija@uchicago.edu>
2020-10-26 22:16:00 -0500

    bash script with more CMD-line args

--- run_train.sh
+++ run_train.sh
@@ -1,7 +1,9 @@
 parallel --bar "python train.py \
     --data {1} \
+    --save_dir out_results/ \
     --subspace_dim {2} \
-    --log_memory 1 \
+    --log_memory \
+    --store_matrices \
     --seed 2000" ::: \
-    test/*.npy ::: \
+    seqs/*.npy ::: \
     10 15
