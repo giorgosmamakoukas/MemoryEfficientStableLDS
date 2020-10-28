729d4f170e6ce3018586e6e8c8cf8fc57bf56121
Orest Xherija <orest.xherija@uchicago.edu>
2020-10-27 22:48:39 -0500

    removed --store_matrices default argument

--- run_train.sh
+++ run_train.sh
@@ -3,7 +3,6 @@ parallel --bar "python train.py \
     --save_dir out_results/ \
     --subspace_dim {2} \
     --log_memory \
-    --store_matrices \
     --seed 2000" ::: \
     seqs/*.npy ::: \
     10 15
