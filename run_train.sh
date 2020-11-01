ca6f722fc0543eb482c20144334443a863f118ac
Orest Xherija <orest.xherija@uchicago.edu>
2020-11-01 17:57:03 -0600

    removed shell script

--- run_train.sh
+++
@@ -1,9 +0,0 @@
-parallel --bar "python train_prediction.py \
-    --data {1} \
-    --save_dir out_results/ \
-    --subspace_dim {2} \
-    --time_limit 1800 \
-    --log_memory \
-    --seed 2000" ::: \
-    seqs/seq_0.npy ::: \
-    10
